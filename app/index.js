524ee92e8e1da5d56beba1fbe3f9d20049a03b71
Lacatusu Mihai Daniel <lacatusu.mihai.daniel@gmail.com>
2016-08-10 14:56:43 +0200

    update example, added timelineOne, controlsWidget, etc

--- app/index.js
+++ app/index.js
@@ -2,8 +2,9 @@ import settings from './config';
 import device from '../lib/helpers/device';
 import dom from '../lib/helpers/dom';
 import Kumullus from '../lib/index';
-import Timeline from '../lib/core/timeline/timeline';
+import Timeline from '../lib/core/timeline/timelineOne';
 import Chapters from '../lib/core/chapters/chapters';
+import controlsWidget from '../lib/core/ui/controlsWidget/controlsWidget';
 
 export default class App extends Kumullus {
 	constructor() {
@@ -17,9 +18,29 @@ export default class App extends Kumullus {
 				{start: 20, end: 50, label: 'Something'},
 				{start: 50, end: this.duration(), label: 'Outro'}
 			]);
+			this.cwidget = new controlsWidget(this);
 		});
+
 		let logo = this.widget(settings.logo);
 		logo.content(document.querySelector('#logo'));
+
+		this.popup = this.popupContainer({className: null});
+
+
+		//popupVideo
+		let popupVideo = this.videoContainer({
+			visible: false,
+			headerBg: null,
+			headerBgAlpha: 0,
+			header: true
+		});
+		popupVideo.setFontSize(60);
+		popupVideo.header.backgroundColor('#000', 0);
+		popupVideo.body.backgroundColor('#000', 1);
+
+		this.popupVideo = popupVideo;
+		this.popupVideo.show();
+		this.popupVideo.player.load('http://client.kumullus.com/biocodex/video.mp4');
 
 		this.emit('resize');
 
--- temp/index.js
+++ temp/index.js
@@ -1614,6 +1614,47 @@ function procentFromString(v) {
 }
 
 /**
+ * @formatTime
+ *
+ * Format seconds as a time string, H:MM:SS or M:SS
+ * Supplying a guide (in seconds) will force a number of leading zeros
+ * to cover the length of the guide
+ *
+ * @param  {Number} seconds Number of seconds to be turned into a string
+ * @param  {Number} guide   Number (in seconds) to model the string after
+ * @return {String}         Time formatted as H:MM:SS or M:SS
+ * @private
+ * @function formatTime
+ */
+function formatTime(seconds, guide = seconds) {
+	seconds = seconds < 0 ? 0 : seconds;
+	let s = Math.floor(seconds % 60);
+	let m = Math.floor(seconds / 60 % 60);
+	let h = Math.floor(seconds / 3600);
+	const gm = Math.floor(guide / 60 % 60);
+	const gh = Math.floor(guide / 3600);
+
+	// handle invalid times
+	if (isNaN(seconds) || seconds === Infinity) {
+		// '-' is false for all relational operators (e.g. <, >=) so this setting
+		// will add the minimum number of fields specified by the guide
+		h = m = s = '-';
+	}
+
+	// Check if we need to show hours
+	h = h > 0 || gh > 0 ? h + ':' : '';
+
+	// If hours are showing, we may need to add a leading zero.
+	// Always show at least one digit of minutes.
+	m = ((h || gm >= 10) && m < 10 ? '0' + m : m) + ':';
+
+	// Check if leading zero is need for seconds
+	s = s < 10 ? '0' + s : s;
+
+	return h + m + s;
+}
+
+/**
  * Detect if the argument passed is a string
  * @param   { * } v - whatever you want to pass to this function
  * @returns { Boolean } -
@@ -5393,17 +5434,319 @@ class Chapters {
 	}
 }
 
+let defaults$11 = {
+	x: 0,
+	width: '100%',
+	height: '8%',
+	minHeight: 36,
+	popup: false
+};
+function Timeline$1 (parentPlayer, options) {
+	return function () {
+		let Timeline = function () {
+			let settings = deepmerge(defaults$11, options);
+			let fragment = document.createDocumentFragment();
+			let svg = new svgSprite();
+			let playBtn = createElement('button');
+			let playBtnSVG = svg.create('play', playBtn);
+			let duration_el = createElement('div', { class: 'duration' });
+			let duration_ = createElement('span');
+			duration_el.appendChild(duration_);
+			let pc = createElement('div', {
+				'class': 'kmlCuepoints'
+			});
+			fragment.appendChild(playBtn);
+			fragment.appendChild(pc);
+			fragment.appendChild(duration_el);
+			this.cuepointsWrapper = pc;
+			let wrapper = null;
+			if (parentPlayer.timelineContainer != null) {
+				this.wrapper = parentPlayer.timelineContainer(settings);
+				this.wrapper.addClass('one');
+				this.wrapper.content(fragment);
+				wrapper = this.wrapper.wrapper;
+			} else {
+				this.wrapper = createElement('div', {
+					class: "kmlTimeline one",
+					style: "position: absolute; left: 0; width: " + settings.width + "; height: " + settings.height + "; top: auto; bottom: 0;"
+				});
+				parentPlayer.wrapper.appendChild(this.wrapper);
+				wrapper = this.wrapper;
+				this.wrapper.appendChild(fragment);
+			}
+			playBtn.addEventListener('click', () => {
+				parentPlayer.togglePlay();
+				try {
+					playBtn.blur();
+				} catch (e) {}
+			});
+			let forwardCls = "";
+			let replayCls = "";
+			parentPlayer.on('forward', function (v) {
+				if (v) {
+					forwardCls = 'forward_' + v;
+					playBtnSVG.setAttribute('href', '#forward5');
+				} else {
+					setTimeout(() => {
+						parentPlayer.paused() ? playBtnSVG.setAttribute('href', '#play') : playBtnSVG.setAttribute('href', '#pause');
+					}, 250);
+				}
+			});
+			parentPlayer.on('replay', function (v) {
+				if (v) {
+					replayCls = 'replay_' + v;
+					playBtnSVG.setAttribute('href', '#replay5');
+				} else {
+					setTimeout(() => {
+						parentPlayer.paused() ? playBtnSVG.setAttribute('href', '#play') : playBtnSVG.setAttribute('href', '#pause');
+					}, 250);
+				}
+			});
+			let remaining = true;
+			duration_el.addEventListener('mouseenter', () => {
+				remaining = false;
+				duration_.innerHTML = formatTime(parentPlayer.currentTime());
+			});
+			duration_el.addEventListener('mouseleave', () => {
+				remaining = true;
+				let t = parentPlayer.currentTime();
+				let d = parentPlayer.duration();
+				duration_.innerHTML = formatTime(d - t);
+			});
+			parentPlayer.on('timeupdate', function () {
+				let t = this.currentTime();
+				let d = this.duration();
+				if (remaining) {
+					duration_.innerHTML = formatTime(d - t);
+				} else {
+					duration_.innerHTML = formatTime(t);
+				}
+			});
+			parentPlayer.on('play', () => {
+				playBtnSVG.setAttribute('href', '#pause');
+			});
+			parentPlayer.on('pause', () => {
+				playBtnSVG.setAttribute('href', '#play');
+			});
+			parentPlayer.on('resize', () => {
+				this.resize();
+			});
+			let w = 0;
+			let cache_sc = 0;
+			this.resize = () => {
+				let sc = procentFromString(settings.height) * parentPlayer.height() / 100;
+				if (cache_sc != sc && this.wrapper.visible()) {
+					cache_sc = sc;
+					if (sc < settings.minHeight) {
+						w = sc = settings.minHeight;
+						if (this.wrapper.settings) {
+							this.wrapper.settings({
+								height: settings.minHeight + "px"
+							});
+						} else {
+							wrapper.style.height = settings.minHeight + "px";
+						}
+						this.wrapper.emit('height', settings.minHeight + "px");
+						playBtn.style.width = w + "px";
+						duration_el.style.width = pc.style.right = pc.style.left = w + "px";
+						parentPlayer;
+					} else {
+						if (this.wrapper.settings) {
+							this.wrapper.settings({
+								height: settings.height
+							});
+						} else {
+							wrapper.style.height = settings.height;
+						}
+						this.wrapper.emit('height', settings.height);
+						w = sc / parentPlayer.width() * 100;
+						playBtn.style.width = w + "%";
+						duration_el.style.width = pc.style.right = pc.style.left = w + "%";
+					}
+				}
+			};
+			let _closed = false;
+			this.closed = v => {
+				if (typeof v === 'boolean') {
+					if (v) {
+						addClass(wrapper, 'closed');
+						this.wrapper.emit('closed', true);
+					} else {
+						removeClass(wrapper, 'closed');
+						this.wrapper.emit('closed', false);
+					}
+					_closed = v;
+				}
+				return _closed;
+			};
+
+			parentPlayer.on('user-idle', v => {
+				if (v) {
+					if (!parentPlayer.paused()) this.closed(true);
+				} else {
+					this.closed(false);
+				}
+			});
+
+			parentPlayer.on('pause', () => {
+				if (this.closed()) {
+					this.closed(false);
+				}
+			});
+
+			this.wrapper.on('show', () => {
+				this.resize();
+			});
+
+			this.show = () => {
+				this.closed(false);
+				this.wrapper.show();
+			};
+			this.hide = () => {
+				this.wrapper.hide();
+			};
+
+			this.setChapters = (chapters, equalChapters, duration) => {
+				this.chapters = new Chapters(parentPlayer, chapters, equalChapters, duration);
+			};
+		};
+		return new Timeline();
+	}();
+};
+
+let defaults$12 = {
+	visible: true,
+	x: 860,
+	y: 170,
+	width: 200,
+	height: 200
+};
+class controlsWidget {
+	constructor(parentPlayer, options) {
+		let temp = deepmerge(defaults$12, {
+			x: parentPlayer.videoWidth() - defaults$12.width / 2,
+			y: (parentPlayer.videoHeight() - defaults$12.height) / 2
+		});
+		let widget = parentPlayer.widget(deepmerge(temp, options));
+		let widget_template = document.getElementById('controls-widget');
+		widget.content(widget_template);
+		this.el = widget;
+
+		let menu_languages = widget_template.querySelector('.languages');
+		let close_languages = widget_template.querySelector('.close');
+		let btn_languages = menu_languages.querySelectorAll('svg');
+		let btn_volume = widget_template.querySelector('.vol');
+		let icon_volume = btn_volume.querySelector('use');
+		let btn_fullscreen = widget_template.querySelector('.fs');
+		let icon_fullscreen = btn_fullscreen.querySelector('use');
+
+		let fn_reset_btn_actives = function () {
+			for (var i = 0, n = btn_languages.length; i < n; i += 1) {
+				removeClass(btn_languages[i], 'active');
+			}
+		};
+
+		close_languages.addEventListener('click', function () {
+			removeClass(menu_languages, 'active');
+			this.style.display = 'none';
+		});
+
+		//language related
+		for (var i = 0, n = btn_languages.length; i < n; i += 1) {
+			let btn = btn_languages[i];
+			btn.addEventListener('click', function () {
+				if (!hasClass(menu_languages, 'active')) {
+					addClass(menu_languages, 'active');
+					close_languages.style.display = 'block';
+				} else {
+					let lng = this.getAttribute('rel');
+					parentPlayer.emit('language', lng);
+					fn_reset_btn_actives();
+					addClass(this, 'active');
+					removeClass(menu_languages, 'active');
+					close_languages.style.display = 'none';
+				}
+			});
+		}
+
+		//volume related
+		parentPlayer.on('volumechange', function () {
+			let v = this.volume();
+			if (this.muted()) {
+				icon_volume.setAttribute('href', '#volume-mute');
+				icon_volume.setAttribute('xlink:href', '#volume-mute');
+				return;
+			} else {
+				icon_volume.setAttribute('href', '#volume-on');
+				icon_volume.setAttribute('xlink:href', '#volume-on');
+			}
+			if (v < .5) {
+				icon_volume.setAttribute('href', '#volume-middle');
+				icon_volume.setAttribute('xlink:href', '#volume-middle');
+			} else {
+				icon_volume.setAttribute('href', '#volume-on');
+				icon_volume.setAttribute('xlink:href', '#volume-on');
+			}
+			if (v == 0) {
+				icon_volume.setAttribute('href', '#volume-off');
+				icon_volume.setAttribute('xlink:href', '#volume-off');
+			}
+		});
+		btn_volume.addEventListener('click', () => {
+			parentPlayer.toggleMute();
+			try {
+				btn_volume.blur();
+			} catch (e) {}
+		});
+
+		//fullscreen related
+		parentPlayer.on('enterFullScreen', function () {
+			icon_fullscreen.setAttribute('href', '#fullscreen-exit');
+			icon_fullscreen.setAttribute('xlink:href', '#fullscreen-exit');
+		});
+		parentPlayer.on('exitFullScreen', function () {
+			icon_fullscreen.setAttribute('href', '#fullscreen');
+			icon_fullscreen.setAttribute('xlink:href', '#fullscreen');
+		});
+		btn_fullscreen.addEventListener('click', () => {
+			parentPlayer.toggleFullScreen();
+			try {
+				btn_sullscreen.blur();
+			} catch (e) {}
+		});
+	}
+}
+
 class App extends Kumullus {
 	constructor() {
-		super(settings.player, Timeline);
+		super(settings.player, Timeline$1);
 	}
 	init() {
 		this.once('loadedmetadata', () => {
 			this.emit('resize');
 			this.chapters = new Chapters(this, [{ start: 0, end: 20, label: 'Intro' }, { start: 20, end: 50, label: 'Something' }, { start: 50, end: this.duration(), label: 'Outro' }]);
+			this.cwidget = new controlsWidget(this);
 		});
+
 		let logo = this.widget(settings.logo);
 		logo.content(document.querySelector('#logo'));
+
+		this.popup = this.popupContainer({ className: null });
+
+		//popupVideo
+		let popupVideo = this.videoContainer({
+			visible: false,
+			headerBg: null,
+			headerBgAlpha: 0,
+			header: true
+		});
+		popupVideo.setFontSize(60);
+		popupVideo.header.backgroundColor('#000', 0);
+		popupVideo.body.backgroundColor('#000', 1);
+
+		this.popupVideo = popupVideo;
+		this.popupVideo.show();
+		this.popupVideo.player.load('http://client.kumullus.com/biocodex/video.mp4');
 
 		this.emit('resize');
 
