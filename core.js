1912d7e5c25d63a3ea137d8a05283f2c11c308e4
Lacatusu Mihai Daniel <lacatusu.mihai.daniel@gmail.com>
2016-11-08 22:06:42 +0100

    init boilerplat, init general css control

--- core.js
+++ core.js
@@ -16,7 +16,6 @@ let args = process.argv.slice(2);
 let __action__ = args[0] || "build";
 let __env__ = Object.create(process.env);
 __env__.NODE_ENV = process.env.NODE_ENV || 'development';
-let __build_language__ = args[1] || 'es6';
 
 let _logSuccess = function(msg, title) {
   var date = new Date;
@@ -105,15 +104,9 @@ processBeforeClose(function() {
 function rollup(done, error) {
   let er = false;
   var ls = null;
-  if(__build_language__ === "es6"){
-    ls = spawn("npm", ['run', 'rollup'], {
-      env: __env__
-    });
-  }else{
       ls = spawn("npm", ['run', 'rollup:ts'], {
       env: __env__
     });
-  }
   _logSuccess("start", 'ROLLUP');
   ls.stdout.on('data', (data) => {
     let d = data.toString();
@@ -233,16 +226,13 @@ function scorm(done) {
 function init() {
   fs.ensureDirSync("./build");
   fs.stat('./app/index.js', function(err) {
-    _logSuccess(__build_language__, 'LANGUAGE');
     _logSuccess(__env__.NODE_ENV, 'ENVIRONMENT');
     if (err) {
       _logSuccess('boilerplating', 'INIT');
-      fs.copySync('./lib/index.html', './app/index.html');
-      if(__build_language__ == "es6"){
-        fs.ensureFileSync('./app/index.js');
-      }else{
-        fs.ensureFileSync('./app/index.ts');
-      }
+      fs.copySync('./lib/boilerplate/index.html', './app/index.html');
+      fs.copySync('./lib/boilerplate/index.js', './app/index.js');
+      fs.copySync('./lib/boilerplate/index.sss', './app/index.sss');
+      fs.copySync('./lib/boilerplate/_colors.sss', './app/_colors.sss');
     }
     switch (__action__) {
       case "serve":
